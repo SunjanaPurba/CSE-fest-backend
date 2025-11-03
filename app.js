b2739f1a6b362aedf82975c40b3d40b8d5a515e3
puspita <puspitabanik213@gmail.com>
2025-11-03 16:50:13 +0600

    feat: new update

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
-const express = require("express");
+import express from "express";
+import dns from "dns";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./db/connect.js";
+import products_routes from "./routes/products.js";
+import facilities_routes from "./routes/facilityRoutes.js";
+
+dotenv.config();
+
 const app = express();
-const connectDB = require("./db/connect");
-const dns = require("dns");
-const cors = require('cors');
-require("dotenv").config();
-
-
-// DNS override
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 app.use(express.json());
 app.use(cors());
 
 const PORT = process.env.PORT || 5000;
-const products_routes = require("./routes/products");
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
 app.use("/api/products", products_routes);
+app.use("/api/facilities", facilities_routes);
 
 const start = async () => {
     try {
---
+++ routes/facilityRoutes.js
@@ -0,0 +1,42 @@
+const express = require("express")
+const router = express.Router()
+
+const facilities = [
+    {
+    id: 1,
+    name: "চর কলাপাড়া কমিউনিটি ক্লিনিক",
+    type: "কমিউনিটি ক্লিনিক",
+    upazila: "গলাচিপা",
+    union: "চর কলাপাড়া",
+    open: "মঙ্গলবার, বৃহস্পতিবার সকাল ৯টা–১২টা",
+    lat: 22.345,
+    lon: 90.235
+  },
+  {
+    id: 2,
+    name: "ব্র্যাক হেলথ কর্নার",
+    type: "এনজিও স্বাস্থ্যকেন্দ্র",
+    upazila: "গলাচিপা",
+    union: "চর মন্টাজ",
+    open: "প্রতিদিন সকাল ৮টা–সন্ধ্যা ৫টা",
+    lat: 22.348,
+    lon: 90.240
+  },
+  {
+    id: 3,
+    name: "রফিক মেডিসিন হাউস",
+    type: "ফার্মেসি",
+    upazila: "গলাচিপা",
+    union: "চর কলাপাড়া",
+    open: "প্রতিদিন ২৪ ঘন্টা",
+    lat: 22.350,
+    lon: 90.245
+  }
+]
+
+router.get("/", (req, res) => {
+    res.json(facilities)
+})
+
+module.exports = router;
+
